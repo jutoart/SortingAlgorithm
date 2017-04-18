d6f77d15835d0483ada8dd824be2a445ba410f0a
jutoart <jutoart@gmail.com>
2017-04-18 12:23:44 +0800

    Initial Commit

---
+++ SortingAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortingAlgorithm
+//
+//  Created by Art Huang on 18/04/2017.
+//  Copyright © 2017 Art Huang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
